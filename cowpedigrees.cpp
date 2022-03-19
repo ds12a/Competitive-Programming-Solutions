1badeac205b4d8550210abd55c87479f2d83c7a6
ds12a <81164854+ds12a@users.noreply.github.com>
2022-03-19 18:32:43 -0400

    Add files via upload

---
+++ cowpedigrees.cpp
@@ -0,0 +1,39 @@
+/*
+ID: david.y3
+LANG: C++
+TASK: nocows
+*/
+#include <fstream>
+#include <iostream>
+
+long long MOD = 9901;
+long long pedigrees[200][100]; // num cows, height
+
+int main()
+{
+    std::ifstream fin("nocows.in");
+    int n, k;
+
+    fin >> n >> k;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= k; j++)
+        {
+            if (i == 1)
+            {
+                pedigrees[i][j] = 1;
+            }
+            else
+            {
+                for (int l = 1; l <= i - 2; l++)
+                {
+                    pedigrees[i][j] += (pedigrees[l][j - 1] * pedigrees[i - l - 1][j - 1]) % MOD;
+                    pedigrees[i][j] %= MOD;
+                }
+            }
+        }
+    }
+    std::ofstream fout("nocows.out");
+    fout << ((pedigrees[n][k] - pedigrees[n][k - 1] + MOD) % MOD) << '\n';
+}
