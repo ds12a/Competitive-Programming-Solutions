97fd066ee778867d0479b662fc9260177f1eb124
ds12a <81164854+ds12a@users.noreply.github.com>
2022-05-25 20:03:14 -0400

    Add files via upload

---
+++ USACO Training Page/Chapter 4/beefmcnuggets.cpp
@@ -0,0 +1,69 @@
+/*
+ID: david.y3
+LANG: C++17
+TASK: nuggets
+*/
+// NAME                 :   David Shen
+// GROUP                :
+// LAST MODIFIED        :   25 May 2022
+// PROBLEM ID           :   nuggets
+// PROBLEM DESCRIPTION  :   Find maximum number that cannot be represented as a weighted sum of <=10 integers (1...256)
+//                          DP, Chicken McNugget Theorem, NT
+// SOURCES              :   USACO Website
+// PEOPLE WHO HELPED ME :
+// PEOPLE I HELPED      :
+
+#include <fstream>
+#include <numeric>
+#include <vector>
+
+bool reachable[587265];
+
+int main()
+{
+    std::ifstream fin("nuggets.in");
+    std::vector<int> packages;
+    int n, x, g = -1;
+
+    fin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        fin >> x;
+
+        packages.push_back(x);
+
+        if (g == -1)
+            g = x;
+        else
+            g = std::gcd(g, x);
+    }
+
+    std::ofstream fout("nuggets.out");
+
+    if (g != 1)
+    {
+        fout << "0\n";
+        return 0;
+    }
+
+    int maxCannotReach = 0;
+    reachable[0] = true;
+
+    for (int i = 0; i <= 587264; i++)
+    {
+        if (reachable[i])
+        {
+            for (int p : packages)
+            {
+                reachable[i + p] = true;
+            }
+        }
+        else
+        {
+            maxCannotReach = i;
+        }
+    }
+
+    fout << maxCannotReach << '\n';
+}
